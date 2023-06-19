f4f00216de9cd818c09adfe36d57d781b57ec4ef
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-19 22:31:56 +0530

    Update user details

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -8,10 +8,10 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    username: "adarshbalika",
-    password: "adarshBalika123",
+    firstName: "Sanika",
+    lastName: "Suryawanshi",
+    username: "sanika31",
+    password: "sanika3103",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
