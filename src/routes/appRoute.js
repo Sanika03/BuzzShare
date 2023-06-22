6ce2a42907dee2d168f60da772a87ae0d495d86d
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-23 00:49:31 +0530

    Add routes

---
+++ src/routes/appRoute.js
@@ -0,0 +1,49 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+// pages
+import  { Home }  from "../pages/home";
+import { Explore } from "../pages/explore";
+import  { Bookmark } from "../pages/bookmark";
+import { UserProfile } from "../pages/userProfile";
+import { Login } from "../pages/auth/login";
+import { Signup } from "../pages/auth/signup";
+
+// components
+import { PrivateRoute } from "./privateRoute";
+import { ScrollToTop } from "../component/scrollToTop";
+import { SinglePost } from "../pages/singlePost";
+import { useAuth } from "../contexts/authContext";
+
+export const AppRoutes = () => {
+  const { token } = useAuth;
+        
+  return (
+    <>
+      <ScrollToTop />
+        <Routes>
+          <Route element={<PrivateRoute />}>
+          <Route path="*" element={<Navigate to="/" replace />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/post/:postId" element={<SinglePost />} />
+
+
+            <Route path="/bookmarks" element={<Bookmark />} />
+            <Route path="/profile/:username" element={<UserProfile />} />
+          </Route>
+       
+          {!token ? (
+            <>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </>
+          ) : (
+            <>
+              <Route path="/login" element={<Navigate to="/" replace />} />
+              <Route path="/signup" element={<Navigate to="/" replace />} />
+            </>
+          )}
+        </Routes>
+    </>
+  );
+};
