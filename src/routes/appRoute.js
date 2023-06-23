1f9a86d4661cdf00b02b223bc3a8e9a265be618d
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-23 16:32:55 +0530

    Remove conditional routing

--- src/routes/appRoute.js
+++ src/routes/appRoute.js
@@ -12,37 +12,21 @@ import { Signup } from "../pages/auth/signup";
 import { PrivateRoute } from "./privateRoute";
 import { ScrollToTop } from "../component/scrollToTop";
 import { SinglePost } from "../component/singlePost";
-import { useAuth } from "../contexts/authContext";
 
 export const AppRoutes = () => {
-  const { token } = useAuth;
         
   return (
     <>
       <ScrollToTop />
         <Routes>
-          <Route element={<PrivateRoute />}>
           <Route path="*" element={<Navigate to="/" replace />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/explore" element={<Explore />} />
-            <Route path="/post/:postId" element={<SinglePost />} />
-
-
-            <Route path="/bookmarks" element={<Bookmark />} />
-            <Route path="/profile/:username" element={<UserProfile />} />
-          </Route>
-       
-          {!token ? (
-            <>
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-            </>
-          ) : (
-            <>
-              <Route path="/login" element={<Navigate to="/" replace />} />
-              <Route path="/signup" element={<Navigate to="/" replace />} />
-            </>
-          )}
+          <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
+          <Route path="/explore" element={<PrivateRoute><Explore /></PrivateRoute>} />
+          <Route path="/post/:postId" element={<PrivateRoute><SinglePost /></PrivateRoute>} />
+          <Route path="/bookmarks" element={<PrivateRoute><Bookmark /></PrivateRoute>} />
+          <Route path="/profile/:username" element={<PrivateRoute><UserProfile /></PrivateRoute>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
     </>
   );
