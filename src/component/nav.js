1da637bc3b35dfe32b0f7e78d78a02ffac772fbf
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-23 17:03:18 +0530

    Add link to user profile

--- src/component/nav.js
+++ src/component/nav.js
@@ -3,9 +3,12 @@ import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faCompass, faBookmark } from '@fortawesome/free-solid-svg-icons';
 
+import { useAuth } from "../contexts/authContext";
+
 import "../styles/nav.css";
 
 export const Nav = () => {
+  const {currUser} = useAuth();
   
   const getTitle = () => <div className="app-heading">
     <img src="https://res.cloudinary.com/dnagcmyka/image/upload/v1687499676/BuzzShare_2_eosvva.png" className="app-logo" alt="App logo"/>
@@ -28,10 +31,21 @@ export const Nav = () => {
     <button className="logout-btn">Logout</button>
   </div>
 
+  const getProfileLink = () => <div className="user-profile">
+    <NavLink to={`/profile/${currUser?.username}`} className="nav-link">
+      <img src={currUser?.profileAvatar} alt="User Avatar" className="user-avatar"/>
+      <div className="user-details-container">
+        <p className="user-details">{currUser?.firstName} {currUser?.lastName}</p>
+        <p className="user-details-username">@{currUser?.username}</p>
+      </div>
+    </NavLink>
+  </div>
+
   return (
     <div className="main-nav">
       {getTitle()}
       {getNav()}
+      {getProfileLink()}
     </div>
   );
 }
