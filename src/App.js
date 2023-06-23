2d987cc9ffba43da57aedbbb3433201269516654
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-23 16:33:23 +0530

    Add sections to app

--- src/App.js
+++ src/App.js
@@ -5,23 +5,33 @@ import 'react-toastify/dist/ReactToastify.css';
 import "./styles/app.css";
 
 import { AppRoutes } from "./routes/appRoute";
+import { Nav } from "./component/nav";
+import { SuggestedUsers } from "./component/suggestedUsers";
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <ToastContainer
-          position='bottom-right'
-          autoClose={3000}
-          newestOnTop={false}
-          closeOnClick
-          theme='colored'
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-        />
+      <ToastContainer
+        position='bottom-right'
+        autoClose={3000}
+        newestOnTop={false}
+        closeOnClick
+        theme='colored'
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+      />
+      <div className="main-container">
+        <div className="left-section">
+          <Nav />
+        </div>
+        <div className="middle-section">
+          <AppRoutes />
+        </div>
+        <div className="right-section">
+          <SuggestedUsers />
+        </div>
       </div>
-      <AppRoutes/>
     </div>
   );
 }
