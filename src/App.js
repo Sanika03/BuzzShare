590e2bce84a47baa4ece76d710e35c084af4e051
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-27 14:00:19 +0530

    Remove components

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import 'react-toastify/dist/ReactToastify.css';
 import "./styles/app.css";
 
 import { AppRoutes } from "./routes/appRoute";
-import { Nav } from "./component/nav";
-import { SuggestedUsers } from "./component/suggestedUsers";
 
 function App() {
   return (
@@ -21,17 +19,7 @@ function App() {
         pauseOnFocusLoss
         draggable
       />
-      <div className="main-container">
-        <div className="left-section">
-          <Nav />
-        </div>
-        <div className="middle-section">
-          <AppRoutes />
-        </div>
-        <div className="right-section">
-          <SuggestedUsers />
-        </div>
-      </div>
+        <AppRoutes />
     </div>
   );
 }
