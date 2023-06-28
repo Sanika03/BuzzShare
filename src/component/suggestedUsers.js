3561064f653a946183df2d88217b64555d41bc14
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-28 21:34:30 +0530

    Add functionality to search users

--- src/component/suggestedUsers.js
+++ src/component/suggestedUsers.js
@@ -9,6 +9,7 @@ export const SuggestedUsers = () => {
   const { users } = useUser();
   const { currUser, token } = useAuth();
   const [suggestedUsers, setSuggestedUsers] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
   const navigate = useNavigate();
 
@@ -61,5 +62,40 @@ export const SuggestedUsers = () => {
     </div>
   );
 
-  return <div className="right-container">{getUsers()}</div>;
+  const getSearch = () => <div id="search-container">
+    <input type="search" placeholder="Search users..." id="search-bar" onChange={(e) => setSearchValue(e.target.value)}/>
+  </div>
+
+  const FilteredUsersBox = () => {
+    const searchUsers = users.filter(({firstName, lastName, username}) => firstName.toLowerCase().includes(searchValue.toLowerCase()) || lastName.toLowerCase().includes(searchValue.toLowerCase()) || username.toLowerCase().includes(searchValue.toLowerCase()));
+
+    return (
+      <div id="searched-users-container">
+        {searchUsers.length > 0 ? (searchUsers.map((user) => (
+        <div id="single-s-user-container" key={user._id} >
+          <img
+            src={user?.profileAvatar}
+            alt="User Avatar"
+            className="s-user-avatar"
+            onClick={() => navigate(`/profile/${user?.username}`)}
+          />
+          <div className="user-details-container" onClick={() => navigate(`/profile/${user?.username}`)}>
+            <p className="user-details s" >
+              {user?.firstName} {user?.lastName}
+            </p>
+            <p className="user-details-username s dark">@{user?.username}</p>
+          </div>
+        </div>
+      ))) : <p>No users found</p>}
+      </div>
+    )
+  }
+
+  return (
+    <div className="right-container">
+      {getSearch()}
+      {getUsers()}
+      {searchValue.length > 0 && <FilteredUsersBox/> }
+    </div>
+  );
 };
