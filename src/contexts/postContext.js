984b02d81bd29c72539837bcc6ecd81c6b4675f5
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-30 20:14:27 +0530

    Change status code and add likePostHandler

--- src/contexts/postContext.js
+++ src/contexts/postContext.js
@@ -1,13 +1,11 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { createPostService, getAllPostsService } from "../services/postServices"
+import { createPostService, getAllPostsService, likePostService } from "../services/postServices"
 
 const PostContext = createContext();
 
 const PostProvider = ({children}) => {
-    const [ posts, setPosts ] = useState([]);
+    const [ postData, setPosts ] = useState([]);
     const [selectedOption, setSelectedOption] = useState("Latest");
-
-    console.log(posts)
 
     const getPostsHandler = async () => {
         try {
@@ -21,10 +19,22 @@ const PostProvider = ({children}) => {
         }
     }
 
-    const addPostHandler = async (input, image, token, currUser) => {
+    const addPostHandler = async ({input, image, token, currUser}) => {
         try {
             const {data: { posts }, status} = await createPostService({input, postImage: image, token, user: currUser});
-            if(status === 200) {
+            if(status === 201) {
+                setPosts(posts);
+            }
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+
+    const likePostHandler = async ({_id, token}) => {
+        try {
+            const {data: { posts }, status} = await likePostService({_id, token});
+            if(status === 201) {
                 setPosts(posts);
             }
         }
@@ -38,7 +48,7 @@ const PostProvider = ({children}) => {
     }, [])
 
     return (
-        <PostContext.Provider value={{ posts, addPostHandler, selectedOption, setSelectedOption }}>
+        <PostContext.Provider value={{ postData, addPostHandler, selectedOption, setSelectedOption, likePostHandler }}>
             {children}
         </PostContext.Provider>
     )
