3d5cd4d9f8cdc279179c94b43d118f5a3d63454f
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-30 22:10:26 +0530

    Add handler for getting, adding, removing bookmark

--- src/contexts/userContext.js
+++ src/contexts/userContext.js
@@ -1,10 +1,14 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { getAllUserService } from "../services/userServices"
+import { getAllUserService, getBookmarkService, removeBookmarkService, addBookmarkService } from "../services/userServices"
+import { useAuth } from "./authContext";
 
 const UserContext = createContext();
 
 const UserProvider = ({children}) => {
-    const [ users, setUsers ] = useState([])
+    const [ users, setUsers ] = useState([]);
+    const [ bookmarks, setBookmarks ] = useState([]);
+    const { token } = useAuth();
+
     const getUserHandler = async () => {
         try {
             const {data: { users }, status} = await getAllUserService();
@@ -17,12 +21,49 @@ const UserProvider = ({children}) => {
         }
     }
 
+    const getBookmarksHandler = async () => {
+        try {
+            const {data: { bookmarks }, status} = await getBookmarkService(token);
+            if(status === 200) {
+                setBookmarks(bookmarks);
+            }  
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+
+    const addBookmarkHandler = async (token, _id) => {
+        try {
+            const {data: { bookmarks }, status} = await addBookmarkService({token, _id});
+            if(status === 200) {
+                setBookmarks(bookmarks);
+            }  
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+
+    const removeBookmarkHandler = async (token, _id) => {
+        try {
+            const {data: { bookmarks }, status} = await removeBookmarkService({token, _id});
+            if(status === 200) {
+                setBookmarks(bookmarks);
+            }  
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+
     useEffect(() => {
         getUserHandler();
+        getBookmarksHandler()
     }, [])
 
     return (
-        <UserContext.Provider value={{ users }}>
+        <UserContext.Provider value={{ users, bookmarks, addBookmarkHandler, removeBookmarkHandler }}>
             {children}
         </UserContext.Provider>
     )
