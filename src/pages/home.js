3a7e43006cedc20fa09aa5f85bcc3aaf55aaf73f
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-06-30 08:48:31 +0530

    Add user posts

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,15 +4,34 @@ import { Nav } from "../component/nav";
 import { SuggestedUsers } from "../component/suggestedUsers";
 import { NewPost } from "../component/newPost";
 import { SortPosts } from "../component/sort";
+import { Post } from "../component/post";
 
 import "../styles/home.css";
+import { usePost } from "../contexts/postContext";
+import { useAuth } from "../contexts/authContext";
 
 export const Home = () => {
+    const { posts } = usePost();
+    const { currUser } = useAuth();
+
     const getTitle = () => (
         <div className="title-container">
         <p className="text">Home</p>
         </div>
     );
+
+    const getPosts = () => {
+        const followingUsernames = currUser.following.map((user) => user.username);
+        const homePosts = posts.filter((post) => {
+          return followingUsernames.includes(post.username) || post.username === currUser.username;
+        });
+      
+        return homePosts.map((post) => (
+          <div className="home-posts-container">
+            <Post post={post} />
+          </div>
+        ));
+    };
 
     return (
         <div className="home-page">
@@ -22,6 +41,7 @@ export const Home = () => {
             <div className="sub-containers">
                 <NewPost />
                 <SortPosts/>
+                {getPosts()}
             </div>
         </div>
         <SuggestedUsers />
