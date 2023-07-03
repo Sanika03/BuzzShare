d8cc350d12daa78367131e7de7c6dc47d6bd167e
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-07-03 15:24:57 +0530

    Add page for all users' posts

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -1,13 +1,35 @@
 import { Nav } from "../component/nav";
+import { Post } from "../component/post";
 import { SuggestedUsers } from "../component/suggestedUsers";
+import { usePost } from "../contexts/postContext";
+
+import "../styles/explore.css";
 
 export const Explore = () => {
-    return (
-        <div>
-            <Nav />
+    const { postData } = usePost();
 
-            This is Explore
-            
+    const getTitle = () => (
+        <div className="title-container">
+        <p className="text">Explore</p>
+        </div>
+    );
+
+    const getExplorePosts = () =>
+        postData.map((post) => (
+          <div className="posts-container" key={post._id}>
+            <Post post={post} />
+          </div>
+        ));
+    
+    return (
+        <div className="explore-page">
+            <Nav />
+            <div className="explore-container">
+                {getTitle()}
+                <div className="sub-containers">
+                    {getExplorePosts()}
+                </div>
+            </div>
             <SuggestedUsers />
         </div>
     )
