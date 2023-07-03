f35eed749cd7b91421c11d862f1fdf23e95663fc
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-07-03 15:24:16 +0530

    Add page for bookmarked posts

--- src/pages/bookmark.js
+++ src/pages/bookmark.js
@@ -1,14 +1,34 @@
 import { Nav } from "../component/nav";
+import { Post } from "../component/post";
 import { SuggestedUsers } from "../component/suggestedUsers";
+import { useUser } from "../contexts/userContext";
+
+import "../styles/bookmarks.css";
 
 export const Bookmark = () => {
-    console.log("bookmark")
-    return (
-        <div>
-            <Nav />
+    const { bookmarks } = useUser();
 
-            This is Bookmark
-            
+    const getTitle = () => (
+        <div className="title-container">
+        <p className="text">Bookmarks</p>
+        </div>
+    );
+
+    const getBookmarkedPosts = () =>
+        bookmarks.map((post) => (
+          <div className="posts-container" key={post._id}>
+            <Post post={post} />
+          </div>
+        ));
+    return (
+        <div className="bookmarks-page">
+            <Nav />
+            <div className="bookmarks-container">
+                {getTitle()}
+                <div className="sub-containers">
+                    {bookmarks.length > 0 ? getBookmarkedPosts() : <h3 className="center">No Bookmarks</h3>}
+                </div>
+            </div>
             <SuggestedUsers />
         </div>
     )
