e8b4773d722540673e153aeb4ba5984e2ac1c48f
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-07-10 14:39:50 +0530

    Remove import and route for single post page

--- src/routes/appRoute.js
+++ src/routes/appRoute.js
@@ -11,7 +11,6 @@ import { Signup } from "../pages/auth/signup";
 // components
 import { PrivateRoute } from "./privateRoute";
 import { ScrollToTop } from "../component/scrollToTop";
-import { SinglePost } from "../component/singlePost";
 
 export const AppRoutes = () => {
         
@@ -22,7 +21,6 @@ export const AppRoutes = () => {
           <Route path="*" element={<Navigate to="/" replace />} />
           <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
           <Route path="/explore" element={<PrivateRoute><Explore /></PrivateRoute>} />
-          <Route path="/post/:postId" element={<PrivateRoute><SinglePost /></PrivateRoute>} />
           <Route path="/bookmarks" element={<PrivateRoute><Bookmark /></PrivateRoute>} />
           <Route path="/profile/:username" element={<PrivateRoute><UserProfile /></PrivateRoute>} />
           <Route path="/login" element={<Login />} />
