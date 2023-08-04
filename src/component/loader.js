6baa79b32ac564acacb3375abb71f6dc9582f61f
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-08-04 16:33:27 +0530

    Add Loader component to display loader gif

---
+++ src/component/loader.js
@@ -0,0 +1,5 @@
+import "../styles/loader.css";
+
+export const Loader = () => <div className="loader-container">
+    <img src="https://res.cloudinary.com/dnagcmyka/image/upload/v1691128579/Eclipse-1s-200px_rkvjv9.gif" alt="loader"/>
+</div>
