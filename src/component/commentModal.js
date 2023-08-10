8e9ce1cf43520aaaea0c69807e15f8c355f99d5c
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-08-11 00:43:46 +0530

    Wrap Modal in a div

--- src/component/commentModal.js
+++ src/component/commentModal.js
@@ -20,30 +20,32 @@ export const CommentModal = ({commentModalOpen, setCommentModalOpen, userProfile
     }
 
     return (
-        <Modal
-            isOpen={commentModalOpen}
-            contentLabel="Comment Modal"
-            onRequestClose={() => setCommentModalOpen(false)}
-            className="comment-modal"
-            shouldCloseOnOverlayClick={true}
-            style={{overlay: { background: "#00000080" } }}
-        >
-            <div className="horizontal">
-                <img
-                    src={userProfile}
-                    alt="User Avatar"
-                    className="s-user-avatar"
-                />
-                <textarea type="text" placeholder="Post your reply" className="comment-input" onChange={(e) => setComment(e.target.value)} />
-            </div>
-            <div className="horizontal right">
-                <button className="form-btn cancel" onClick={() => handleCancel()}>
-                    Cancel
-                </button>
-                <button className="form-btn save" onClick={() => handleSave()}>
-                    Reply
-                </button>
-            </div>
-        </Modal>
+        <div onClick={(e) => e.stopPropagation()}>
+            <Modal
+                isOpen={commentModalOpen}
+                contentLabel="Comment Modal"
+                onRequestClose={() => setCommentModalOpen(false)}
+                className="comment-modal"
+                shouldCloseOnOverlayClick={true}
+                style={{overlay: { background: "#00000080" } }}
+            >
+                <div className="horizontal"> 
+                    <img
+                        src={userProfile}
+                        alt="User Avatar"
+                        className="s-user-avatar"
+                    />
+                    <textarea type="text" placeholder="Post your reply" className="comment-input" onChange={(e) => setComment(e.target.value)} />
+                </div>
+                <div className="horizontal right">
+                    <button className="form-btn cancel" onClick={() => handleCancel()}>
+                        Cancel
+                    </button>
+                    <button className="form-btn save" disabled={!comment.length > 0} style={{cursor: comment.length > 0 ? "pointer" : "not-allowed"}} onClick={() => handleSave()}>
+                        Reply
+                    </button>
+                </div>
+            </Modal>
+        </div>
     )
 }
